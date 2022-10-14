a294906a63efe2fe9bd4a68be0581eaa1b62c553
Tim Vande Walle <tim@libaro.be>
2022-10-14 15:41:33 +0200

    first commit

---
+++ config/slow-queries.php
@@ -0,0 +1,7 @@
+<?php
+// config for libaro/LaravelSlowQueries
+return [
+    'enabled' => env('LARAVEL_SLOW_QUERIES_ENABLED', false),                            // disabled by default
+    'debug' => env('LARAVEL_SLOW_QUERIES_DEBUG', false),                                // disabled by default
+    'log_queries_slower_than' => env('LARAVEL_SLOW_QUERIES_LOG_SLOWER_THAN', 0.5)       // log queries that are slower than x,  in seconds
+];
