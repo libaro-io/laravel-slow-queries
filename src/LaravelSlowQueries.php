5631fb7f53149f566f71fb3064a5729e157c1d09
Tim Vande Walle <tim@libaro.be>
2022-10-21 15:42:51 +0200

    main class for handling the queries

--- src/LaravelSlowQueries.php
+++ src/LaravelSlowQueries.php
@@ -2,35 +2,139 @@
 
 namespace Libaro\LaravelSlowQueries;
 
+use Illuminate\Database\Events\QueryExecuted;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
+use Libaro\LaravelSlowQueries\Models\SlowQuery;
 
 class LaravelSlowQueries
 {
+
+    /**
+     * @var Request
+     */
+    private Request $request;
+
+    /**
+     * @var SlowQuery
+     */
+    private SlowQuery $slowQuery;
+
+    /**
+     * @return bool
+     */
+    public function isPackageEnabled(): bool
+    {
+        return (bool)config('slow-queries.enabled');
+    }
+
+    /**
+     * @return float
+     */
+    public function getSlowerThan(): float
+    {
+        $slowerThan = config('slow-queries.log_queries_slower_than');
+        return is_numeric($slowerThan) ? (float)$slowerThan : 0;
+    }
+
     /**
      * @return void
      */
-    public function startListeningWhenEnabled(): void
+    public function startListening()
     {
-        if ($this->isPackageEnabled()) {
-            $this->startListening();
+        DB::listen(function (QueryExecuted $queryExecuted) {
+            $this->setRequest();
+
+            $this->slowQuery = $this->getDataFromQueryExecuted($queryExecuted);
+            $this->saveSlowQuery();
+        });
+    }
+
+    /**
+     * @return void
+     */
+    private function saveSlowQuery(): void
+    {
+        if ($this->isQuerySlow() && !$this->isQueryMetaQuery()) {
+            $this->slowQuery->save();
         }
     }
 
     /**
      * @return void
      */
-    private function startListening(): void
+    private function setRequest(): void
     {
-        Log::debug('testing package');
+        $request = request();
 
+        if ($request instanceof Request) {
+            $this->request = $request;
+        }
+    }
 
+    /**
+     * @param QueryExecuted $queryExecuted
+     * @return SlowQuery
+     */
+    private function getDataFromQueryExecuted(QueryExecuted $queryExecuted): SlowQuery
+    {
+        $slowQuery = new SlowQuery();
+        $slowQuery->uri = $this->getUri();
+        $slowQuery->action = $this->getAction();
+        $slowQuery->query = $this->getQueryWithParams($queryExecuted);
+        $slowQuery->duration = $this->getDuration($queryExecuted);
+
+        return $slowQuery;
+    }
+
+    /**
+     * @param QueryExecuted $query
+     * @return string
+     */
+    private function getQueryWithParams(QueryExecuted $query): string
+    {
+        return Str::replaceArray('?', $query->bindings, $query->sql);
+    }
+
+    /**
+     * @return string
+     */
+    private function getUri(): string
+    {
+        return $this->request->getRequestUri();
+    }
+
+    /**
+     * @return string
+     */
+    private function getAction(): string
+    {
+        return '';
+    }
+
+    /**
+     * @param QueryExecuted $query
+     * @return float
+     */
+    private function getDuration(QueryExecuted $query): float
+    {
+        return $query->time;
     }
 
     /**
      * @return bool
      */
-    private function isPackageEnabled()
+    private function isQuerySlow(): bool
     {
-        return (bool)config('slow-queries.enabled');
+        return $this->slowQuery->duration >= $this->getSlowerThan();
     }
+
+    private function isQueryMetaQuery(): bool
+    {
+        return str_contains($this->slowQuery->query, 'slow_queries');
+    }
+
+
 }
