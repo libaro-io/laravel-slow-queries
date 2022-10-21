6cd086912da0cb4bdccc26f13c1e3ab14272c6cd
Tim Vande Walle <tim@libaro.be>
2022-10-21 18:03:35 +0200

    code cleanup

--- src/LaravelSlowQueriesServiceProvider.php
+++ src/LaravelSlowQueriesServiceProvider.php
@@ -6,9 +6,6 @@ use Illuminate\Support\ServiceProvider;
 
 class LaravelSlowQueriesServiceProvider extends ServiceProvider
 {
-
-
-
     public function boot(): void
     {
         $this->publishes([
@@ -40,15 +37,4 @@ class LaravelSlowQueriesServiceProvider extends ServiceProvider
             $laravelSlowQueries->startListening();
         }
     }
-
-//    /**
-//     * @return void
-//     */
-//    private function registerTerminating(): void
-//    {
-//        $laravelSlowQueries = new LaravelSlowQueries();
-//        if ($laravelSlowQueries->isPackageEnabled()) {
-//            $laravelSlowQueries->registerTerminating();
-//        }
-//    }
 }
