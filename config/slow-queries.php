fcdb96fba6f0cedb2edced7deeb32ac4acbf071c
Tim Vande Walle <tim@libaro.be>
2022-10-28 13:45:08 +0200

    config update

--- config/slow-queries.php
+++ config/slow-queries.php
@@ -3,5 +3,5 @@
 return [
     'enabled' => env('LARAVEL_SLOW_QUERIES_ENABLED', false),                            // disabled by default
     'debug' => env('LARAVEL_SLOW_QUERIES_DEBUG', false),                                // disabled by default
-    'log_queries_slower_than' => env('LARAVEL_SLOW_QUERIES_LOG_SLOWER_THAN', 0.5)       // log queries that are slower than x,  in seconds
+    'log_queries_slower_than' => env('LARAVEL_SLOW_QUERIES_LOG_SLOWER_THAN', 10)       // log queries that are slower than x,  in miliseconds
 ];
