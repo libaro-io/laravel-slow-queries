43cc7ae47848d35a522332f1695ce3677eed1baa
Johan Cleve <johan@libaro.be>
2022-10-28 15:43:22 +0200

    Add config paramaters

--- config/slow-queries.php
+++ config/slow-queries.php
@@ -3,5 +3,7 @@
 return [
     'enabled' => env('LARAVEL_SLOW_QUERIES_ENABLED', false),                            // disabled by default
     'debug' => env('LARAVEL_SLOW_QUERIES_DEBUG', false),                                // disabled by default
+    'middleware' => 'web',
+    'url-prefix' => '/slow-queries',
     'log_queries_slower_than' => env('LARAVEL_SLOW_QUERIES_LOG_SLOWER_THAN', 10)       // log queries that are slower than x,  in miliseconds
 ];
