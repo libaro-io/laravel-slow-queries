6c548ca4f2408200d67aa00c417c079ad23c8020
Johan Cleve <johan@libaro.be>
2022-11-04 14:26:04 +0100

    Add dashboard view

--- src/Http/Controllers/DashboardController.php
+++ src/Http/Controllers/DashboardController.php
@@ -2,10 +2,13 @@
 
 namespace Libaro\LaravelSlowQueries\Http\Controllers;
 
+use Libaro\LaravelSlowQueries\Models\SlowQuery;
+
 class DashboardController extends Controller
 {
     public function show()
     {
-        // TODO: Implement
+        $queries = SlowQuery::paginate();
+        return view('slow-queries::dashboard.show', ['title' => 'Dashboard', 'queries' => $queries]);
     }
 }
