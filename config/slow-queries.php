78623552db0b0b65ac08105bd04d38ca53933038
Tim Vande Walle <tim@libaro.be>
2022-11-04 14:28:29 +0100

    better default values

--- config/slow-queries.php
+++ config/slow-queries.php
@@ -3,9 +3,8 @@
 return [
     'enabled' => env('LARAVEL_SLOW_QUERIES_ENABLED', false),                            // disabled by default
     'debug' => env('LARAVEL_SLOW_QUERIES_DEBUG', false),                                // disabled by default
-    'log_queries_slower_than' => env('LARAVEL_SLOW_QUERIES_LOG_SLOWER_THAN', 10),       // log queries that are slower than x,  in miliseconds
-    'log_queries_more_than' => env('LARAVEL_SLOW_QUERIES_LOG_MORE_THAN', 10)            // log queries if there are more than x in the request
+    'log_queries_slower_than' => env('LARAVEL_SLOW_QUERIES_LOG_SLOWER_THAN', 500),       // log queries that are slower than x,  in miliseconds
+    'log_queries_more_than' => env('LARAVEL_SLOW_QUERIES_LOG_MORE_THAN', 50),           // log queries if there are more than x in the request
     'middleware' => 'web',
     'url-prefix' => '/slow-queries',
-    'log_queries_slower_than' => env('LARAVEL_SLOW_QUERIES_LOG_SLOWER_THAN', 10)       // log queries that are slower than x,  in miliseconds
 ];
