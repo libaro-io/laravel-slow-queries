f63e119d387213c20a68fa85d1c752a8504e063a
Tim Vande Walle <tim@libaro.be>
2022-11-04 14:27:21 +0100

    added config option : more than x slow queries

--- config/slow-queries.php
+++ config/slow-queries.php
@@ -3,6 +3,8 @@
 return [
     'enabled' => env('LARAVEL_SLOW_QUERIES_ENABLED', false),                            // disabled by default
     'debug' => env('LARAVEL_SLOW_QUERIES_DEBUG', false),                                // disabled by default
+    'log_queries_slower_than' => env('LARAVEL_SLOW_QUERIES_LOG_SLOWER_THAN', 10),       // log queries that are slower than x,  in miliseconds
+    'log_queries_more_than' => env('LARAVEL_SLOW_QUERIES_LOG_MORE_THAN', 10)            // log queries if there are more than x in the request
     'middleware' => 'web',
     'url-prefix' => '/slow-queries',
     'log_queries_slower_than' => env('LARAVEL_SLOW_QUERIES_LOG_SLOWER_THAN', 10)       // log queries that are slower than x,  in miliseconds
