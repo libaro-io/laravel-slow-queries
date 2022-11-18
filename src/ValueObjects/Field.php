d18928dc5ddf10bd2e5876e27f0975f7341aef5a
Tim Vande Walle <tim@libaro.be>
2022-11-18 17:48:27 +0100

    valueobject for field

---
+++ src/ValueObjects/Field.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Libaro\LaravelSlowQueries\ValueObjects;
+
+class Field
+{
+    /**
+     * @var string
+     */
+    public $tableNameOrAlias;
+
+    /**
+     * @var string
+     */
+    public $fieldName;
+
+    /**
+     * @var string
+     */
+    public $fullName;
+
+
+
+}
