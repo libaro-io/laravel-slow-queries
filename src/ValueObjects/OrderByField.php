a241d5d659224f089213d77e32af5831299d87ae
Tim Vande Walle <tim@libaro.be>
2022-11-18 17:49:10 +0100

    added orderByField as sub of Field

--- src/ValueObjects/OrderByField.php
+++ src/ValueObjects/OrderByField.php
@@ -2,23 +2,10 @@
 
 namespace Libaro\LaravelSlowQueries\ValueObjects;
 
-class Field
+class OrderByField extends Field
 {
     /**
      * @var string
      */
-    public $tableNameOrAlias;
-
-    /**
-     * @var string
-     */
-    public $fieldName;
-
-    /**
-     * @var string
-     */
-    public $fullName;
-
-
-
+    public $orderDirection;
 }
