decf8858078f3fe73d7daf91bd85ec762e7606b3
Tim Vande Walle <tim@libaro.be>
2022-11-18 16:30:14 +0100

    wrapper class (valueobject) for WhereField

---
+++ src/ValueObjects/WhereField.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Libaro\LaravelSlowQueries\ValueObjects;
+
+class TableAlias
+{
+    /**
+     * @var string
+     */
+    public $tableName;
+
+    /**
+     * @var string
+     */
+    public $alias;
+
+
+
+}
