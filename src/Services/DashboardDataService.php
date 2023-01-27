229f2b3dc17749eaa4576fe446cc21ec79f7bce8
Tim Vande Walle <tim@libaro.be>
2023-01-27 15:37:48 +0100

    added service to fetch dashboard data

---
+++ src/Services/DashboardDataService.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Libaro\LaravelSlowQueries\Services;
+
+use Illuminate\Support\Carbon;
+
+class DashboardDataService
+{
+
+
+    public function setDateRange(Carbon $from, Carbon $to)
+    {
+
+    }
+
+    public function getNSlowestQueriesForDateRange(int $n)
+    {
+
+    }
+
+}
