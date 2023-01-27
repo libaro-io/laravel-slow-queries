097747e72ab117dafc79ef01a55ece870bd98235
Tim Vande Walle <tim@libaro.be>
2023-01-27 15:49:54 +0100

    added configurable items per widget + default date range

--- config/slow-queries.php
+++ config/slow-queries.php
@@ -7,4 +7,7 @@ return [
     'log_queries_more_than' => env('LARAVEL_SLOW_QUERIES_LOG_MORE_THAN', 50),           // log queries if there are more than x in the request
     'middleware' => 'web',
     'url-prefix' => '/slow-queries',
+
+    'items_per_widget' => env('LARAVEL_SLOW_QUERIES_ITEMS_PER_WIDGET', 5),                           // number of items to show in a dashboard widget
+    'default_date_range' => env('LARAVEL_SLOW_QUERIES_DEFAULT_DATE_RANGE', 14),                           // default date range, in days
 ];
