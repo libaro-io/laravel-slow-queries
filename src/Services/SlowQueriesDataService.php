57253e93a31aa62fa28e0ca88a33ec702e90e494
Tim Vande Walle <tim@libaro.be>
2023-02-03 18:18:56 +0100

    add SlowPagesDataService

---
+++ src/Services/SlowQueriesDataService.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Libaro\LaravelSlowQueries\Services;
+
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
+use Libaro\LaravelSlowQueries\Models\SlowQuery;
+
+// TODO : refactore : create base class for the common methods
+
+/**
+ *
+ */
+class SlowPagesDataService extends BaseDataService
+{
+    
+    /**
+     * @return Collection<int, mixed>
+     */
+    public function get(): Collection
+    {
+        // TODO : filter on date range
+        $sql = /** @lang sql */
+            <<<SQL
+            select the_uri, avg(the_duration) as the_duration, avg(the_count) as the_count, max(request_guid) as the_guid
+            from
+            (
+                select request_guid, sum(duration) as the_duration, count(*) as the_count, min(uri) as the_uri
+                from slow_queries
+                group by request_guid
+                order by 3 desc
+            ) derived
+            
+            group by the_uri
+            order by the_duration desc
+            limit ?
+SQL;
+
+        $records = DB::select($sql, [$this->numberOfItems]);
+        $collection = collect($records);
+
+        return $collection;
+    }
+}
