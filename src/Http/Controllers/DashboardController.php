7d76ae1e2cd8528276fb850d1a4d15c4a623f6b7
Tim Vande Walle <tim@libaro.be>
2023-02-03 15:55:12 +0100

    refactor dashboardcontroller for phpstan T16899

--- src/Http/Controllers/DashboardController.php
+++ src/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace Libaro\LaravelSlowQueries\Http\Controllers;
 
+use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\View;
 use Illuminate\Contracts\View\Factory;
 use Libaro\LaravelSlowQueries\Models\SlowQuery;
@@ -9,7 +10,10 @@ use Libaro\LaravelSlowQueries\Services\DashboardDataService;
 
 class DashboardController extends Controller
 {
-    public function show():  Factory|View
+    /**
+     * @return Application|Factory|View
+     */
+    public function show(): View|Factory|Application
     {
         /** @phpstan-ignore-next-line */
         $queries = SlowQuery::paginate();
