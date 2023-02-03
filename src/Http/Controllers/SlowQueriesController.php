5bfc6d1b265c3c66fe0cdc14f24aaebf352822e1
Tim Vande Walle <tim@libaro.be>
2023-02-03 18:19:50 +0100

    refactor slowqueries controller to make use of dataservice

--- src/Http/Controllers/SlowQueriesController.php
+++ src/Http/Controllers/SlowQueriesController.php
@@ -5,29 +5,17 @@ namespace Libaro\LaravelSlowQueries\Http\Controllers;
 use Illuminate\Contracts\View\View;
 use Illuminate\Contracts\View\Factory;
 use Libaro\LaravelSlowQueries\Models\SlowQuery;
+use Libaro\LaravelSlowQueries\Services\SlowQueriesDataService;
 
 class SlowQueriesController extends Controller
 {
     public function index(): Factory|View
     {
-        $slowQueries = SlowQuery::query()
-            ->groupBy('query_hashed', 'uri')
-            ->select('query_hashed', 'uri')
-            ->selectRaw('min(source_file) as source_file')
-            ->selectRaw('min(line) as min_line')
-            ->selectRaw('max(line) as max_line')
-            ->selectRaw('min(query_without_bindings) as query_without_bindings')
-            ->selectRaw('min(duration) as min_duration')
-            ->selectRaw('max(duration) as max_duration')
-            ->selectRaw('avg(duration) as avg_duration')
-            ->selectRaw('max(id) as id')
-            ->orderByDesc('avg_duration')
+        $slowQueriesDataService = new SlowQueriesDataService();
+        $slowQueriesDataService->setNumberOfItems(999);
+        $slowQueriesDataService->setDateRange();
 
-            ->get();
 
-//        dd($slowQueries);
-
-//        echo("ok");
 
         return view('slow-queries::slow-queries.index', compact('slowQueries'));
     }
