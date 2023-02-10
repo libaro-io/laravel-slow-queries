382b4dd254140100079488d02a0e6439bd7dbf2a
Tim Vande Walle <tim@libaro.be>
2023-02-10 17:30:53 +0100

    Add custom sections to default main layout: custom_js and custom_css

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -9,6 +9,9 @@
     <script src="https://cdn.tailwindcss.com"></script>
 {{--    <script src="https://kit.fontawesome.com/e9e218acb7.js" crossorigin="anonymous"></script>--}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
+    @yield('custom_css')
 </head>
 <body>
     <div>
@@ -75,6 +78,8 @@
             </main>
         </div>
     </div>
+
+@yield('custom_js')
 
 </body>
 </html>
