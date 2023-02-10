8e6b4a3c47f18b75a0a16b69bef9d662160de751
Tim Vande Walle <tim@libaro.be>
2023-02-10 14:59:09 +0100

    Update slow query data object class with the correct properties

--- src/Data/SlowQueryData.php
+++ src/Data/SlowQueryData.php
@@ -2,7 +2,17 @@
 
 namespace Libaro\LaravelSlowQueries\Data;
 
+/* data object to define the data for queries at the first level: grouped by same query */
+
 class SlowQueryData
 {
-
+    public string $queryHashed;
+    public string $uri;
+    public string $sourceFile;
+    public int $minLine;
+    public int $maxLine;
+    public string $queryWithoutBindings;
+    public int $minDuration;
+    public int $maxDuration;
+    public int $avgDuration;
 }
