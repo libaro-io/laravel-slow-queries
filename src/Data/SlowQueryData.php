551a986d970a05a4e4673f9f7363f9044010c9b0
Tim Vande Walle <tim@libaro.be>
2023-02-10 15:31:12 +0100

    Add phpdoc blocks to SlowQueryData data object class for the properties

--- src/Data/SlowQueryData.php
+++ src/Data/SlowQueryData.php
@@ -4,15 +4,54 @@ namespace Libaro\LaravelSlowQueries\Data;
 
 /* data object to define the data for queries at the first level: grouped by same query */
 
+use Illuminate\Support\Collection;
+use Libaro\LaravelSlowQueries\Models\SlowQuery;
+
 class SlowQueryData
 {
+    /**
+     * @var string
+     */
     public string $queryHashed;
+    /**
+     * @var string
+     */
     public string $uri;
+    /**
+     * @var string
+     */
     public string $sourceFile;
+    /**
+     * @var int
+     */
     public int $minLine;
+    /**
+     * @var int
+     */
     public int $maxLine;
+    /**
+     * @var string
+     */
     public string $queryWithoutBindings;
+    /**
+     * @var string
+     */
+    public string $queryWithBindings;
+    /**
+     * @var int
+     */
     public int $minDuration;
+    /**
+     * @var int
+     */
     public int $maxDuration;
+    /**
+     * @var int
+     */
     public int $avgDuration;
+
+    /**
+     * @var Collection<int, SlowQuery>
+     */
+    public Collection $details;
 }
