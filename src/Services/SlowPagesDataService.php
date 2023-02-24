10bc73cd39a9998f44662af4b128ecb97ca73891
Tim Vande Walle <tim@libaro.be>
2023-02-24 15:34:39 +0100

    Add SlowPagesDataService

--- src/Services/SlowPagesDataService.php
+++ src/Services/SlowPagesDataService.php
@@ -2,40 +2,44 @@
 
 namespace Libaro\LaravelSlowQueries\Services;
 
-use Illuminate\Support\Carbon;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\DB;
-use Libaro\LaravelSlowQueries\Models\SlowQuery;
+use Libaro\LaravelSlowQueries\Data\SlowPageAggregation;
+use Libaro\LaravelSlowQueries\Models\SlowPage;
 
 
 class SlowPagesDataService extends BaseDataService
 {
-    
+
     /**
-     * @return Collection<int, mixed>
+     * @return Collection<int, SlowPageAggregation>
      */
-    public function get(): Collection
+    public function getSlowestPagesAggregation(): Collection
     {
-        // TODO : filter on date range
-        $sql = /** @lang sql */
-            <<<SQL
-            select the_uri, avg(the_duration) as the_duration, avg(the_count) as the_count, max(request_guid) as the_guid
-            from
-            (
-                select request_guid, sum(duration) as the_duration, count(*) as the_count, min(uri) as the_uri
-                from slow_queries
-                group by request_guid
-                order by 3 desc
-            ) derived
-            
-            group by the_uri
-            order by the_duration desc
-            limit ?
-SQL;
+        /**
+         * @var Collection<int, SlowPageAggregation> $slowestPagesAggregation
+         */
+        $slowestPagesAggregation = $this
+            ->getBaseQuery()
+            ->orderByDesc('avgDuration')
+            ->limit($this->numberOfItemsPerWidget)
+            ->get();
 
-        $records = DB::select($sql, [$this->numberOfItemsPerWidget]);
-        $collection = collect($records);
+        return $slowestPagesAggregation;
+    }
 
-        return $collection;
+    /**
+     * @return Builder
+     */
+    private function getBaseQuery(): Builder
+    {
+        $builder = SlowPage::query()
+            ->selectRaw('the_uri')
+            ->selectRaw('avg(the_duration) as avgDuration')
+            ->selectRaw('avg(the_count) as the_count')
+            ->groupBy('the_uri');
+
+        /** @phpstan-ignore-next-line */
+        return $builder;
     }
 }
