ef8a7ef90bc26a9461482f4807847fa3ea7cfefb
Tim Vande Walle <tim@libaro.be>
2023-03-03 16:21:59 +0100

    Refactor code cleanup

--- src/Http/Controllers/SlowPagesController.php
+++ src/Http/Controllers/SlowPagesController.php
@@ -6,16 +6,10 @@ use Illuminate\Auth\Access\Response;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Routing\Redirector;
-use Libaro\LaravelSlowQueries\Models\SlowQuery;
 use Libaro\LaravelSlowQueries\Services\SlowPagesDataService;
 
 class SlowPagesController extends Controller
 {
-    /**
-     * @return Factory|View
-     */
     public function index(): Factory|View
     {
         $slowPagesService = new SlowPagesDataService();
@@ -25,7 +19,6 @@ class SlowPagesController extends Controller
     }
 
     /**
-     * @param string $uriBase64Encoded
      * @return bool|Response|Application|Factory|View|null
      */
     public function show(string $uriBase64Encoded)
