2153a271681bc0c271d58d12517637a81213430a
Tim Vande Walle <tim@libaro.be>
2023-03-03 16:21:26 +0100

    Update dashboard to show page count instead of average query count

--- resources/views/dashboard/show.blade.php
+++ resources/views/dashboard/show.blade.php
@@ -108,7 +108,7 @@
                                 {{ceil($slowPage->avgDuration / 1000)}}
                             </td>
                             <td class="px-2 py-4 text-center">
-                                {{$slowQueryAggregation->queryCount}}
+                                {{$slowPage->count}}
                             </td>
                             <td class="px-6 py-4 text-center">
                                 <a href="{{ route('slow-queries.slow-pages.show', ['uriBase64Encoded' => base64_encode($slowPage->uri) ]) }}"><i
