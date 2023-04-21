e759c03e6c05a0c4c78f131f67847a3a549ce1fe
Tim Vande Walle <tim@libaro.be>
2023-04-21 17:44:05 +0200

    Add blade component for time range picker

---
+++ resources/views/components/time-range-picker.blade.php
@@ -0,0 +1,96 @@
+<div class="daterange fw-1/2 flex justify-end" x-data="{ open: false }" >
+    <div class="relative flex justify-end w-full min-w-[200px] border py-3">
+        <button type="button"
+                class="inline-flex justify-between items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900 px-4 w-full"
+                aria-expanded="false"
+                x-on:click="open = ! open"
+
+        >
+            <span class="text-gray-400">Time picker</span>
+            <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path fill-rule="evenodd"
+                      d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                      clip-rule="evenodd"/>
+            </svg>
+        </button>
+
+        <!--
+          Flyout menu, show/hide based on flyout menu state.
+
+          Entering: "transition ease-out duration-200"
+            From: "opacity-0 translate-y-1"
+            To: "opacity-100 translate-y-0"
+          Leaving: "transition ease-in duration-150"
+            From: "opacity-100 translate-y-0"
+            To: "opacity-0 translate-y-1"
+        -->
+        <div class="absolute ftop-0 right-0 fleft-1/2 z-10 mt-12 flex w-[200px] f-translate-x-1/2 fpx-4"
+             x-show="open"
+             @click.outside="open = false">
+            <div class="w-screen max-w-md flex-auto overflow-hidden bg-white text-sm leading-6 shadow-lg ring-1 ring-gray-900/5 lg:max-w-3xl">
+                <div class="grid grid-cols-1 gap-x-6 gap-y-1 p-4 flg:grid-cols-2">
+                    <div class="group relative flex gap-x-6 rounded-lg p-4 fhover:bg-gray-50 w-full ">
+
+                        <ul role="list" class="divide-y divide-gray-100 w-full">
+
+                            @foreach(\Libaro\LaravelSlowQueries\ValueObjects\TimeRanges::getValids() as $timeRange)
+                                <li class="flex gap-x-4 py-5 items-center text-gray-400 w-full hover:text-gray-900 hover:cursor-pointer"
+                                    >
+
+                                    {{--                                    <i class="fa-solid fa-circle-check text-"></i>--}}
+                                    <i class="fa-regular fa-clock"></i>
+
+                                    <div class="min-w-0">
+                                        <p class="text-sm font-semibold leading-6  hover:text-gray-900">{{$timeRange['label']}}</p>
+
+                                    </div>
+                                </li>
+                            @endforeach
+
+                        </ul>
+
+
+                    </div>
+                </div>
+                {{--            <div class="bg-gray-50 px-8 py-6">--}}
+                {{--                <div class="flex items-center gap-x-3">--}}
+                {{--                    <h3 class="text-sm font-semibold leading-6 text-gray-900">Enterprise</h3>--}}
+                {{--                    <p class="rounded-full bg-indigo-600/10 px-2.5 py-1.5 text-xs font-semibold text-indigo-600">New</p>--}}
+                {{--                </div>--}}
+                {{--                <p class="mt-2 text-sm leading-6 text-gray-600">Empower your entire team with even more advanced tools.</p>--}}
+                {{--            </div>--}}
+            </div>
+        </div>
+    </div>
+</div>
+
+@push('custom_js')
+    <script>
+        function sendTimeRange(timeRange) {
+            console.log("sending time range", timeRange);
+
+            // // Replace with your backend API endpoint URL
+            // const url = 'https://example.com/api/save-time-range';
+            //
+            // // Replace with your preferred method (POST, PUT, etc.) and headers
+            // const options = {
+            //     method: 'POST',
+            //     headers: {
+            //         'Content-Type': 'application/json',
+            //     },
+            //     body: JSON.stringify({timeRange}),
+            // };
+            //
+            // // Send the request to the backend
+            // fetch(url, options)
+            //     .then(response => {
+            //         if (!response.ok) {
+            //             throw new Error('Failed to save time range');
+            //         }
+            //     })
+            //     .catch(error => {
+            //         console.error(error);
+            //     });
+        }
+    </script>
+@endpush
