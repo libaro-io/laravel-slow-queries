e6251047d890dacf33ca2f21e52d2cbd461964ac
Tim Vande Walle <tim@libaro.be>
2023-04-21 17:43:35 +0200

    Add valueobject for time ranges

---
+++ src/ValueObjects/TimeRanges.php
@@ -0,0 +1,60 @@
+<?php
+
+
+namespace Libaro\LaravelSlowQueries\ValueObjects;
+
+
+
+class TimeRanges
+{
+    public const THIRTY_MINUTES = 1;
+    public const THREE_HOURS = 3;
+    public const TWELVE_HOURS = 5;
+    public const ONE_DAY = 6;
+    public const ONE_WEEK = 8;
+    public const FOUR_WEEKS = 11;
+
+    /**
+     * @param $type
+     * @return bool
+     */
+    public static function isValid(string $type): bool
+    {
+        return in_array($type, self::getValids(), true);
+    }
+
+    /**
+     * @return int[]
+     */
+    public static function getValids(): array
+    {
+        return [
+            self::THIRTY_MINUTES => [
+                'label' => 'thirty minutes',
+                'duration' => 30
+            ],
+            self::THREE_HOURS => [
+                'label' => 'three hours',
+                'duration' => 60 * 3
+            ],
+            self::TWELVE_HOURS => [
+                'label' => 'twelve hours',
+                'duration' => 60 * 12
+            ],
+            self::ONE_DAY => [
+                'label' => 'one day',
+                'duration' => 60 * 24
+            ],
+            self::ONE_WEEK => [
+                'label' => 'one week',
+                'duration' => 60 * 24 * 7
+            ],
+            self::FOUR_WEEKS => [
+                'label' => 'four weeks',
+                'duration' => 60 * 24 * 7 * 4
+            ]
+        ];
+
+
+    }
+}
