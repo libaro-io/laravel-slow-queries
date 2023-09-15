9c10c5b6ad0a2046f36186848a2fd503f41e008c
Tim Vande Walle <tim@libaro.be>
2023-09-15 18:07:32 +0200

    Update timeranges

--- src/ValueObjects/TimeRanges.php
+++ src/ValueObjects/TimeRanges.php
@@ -7,10 +7,12 @@ namespace Libaro\LaravelSlowQueries\ValueObjects;
 
 class TimeRanges
 {
-    public const THIRTY_MINUTES = 30;
+//    public const THIRTY_MINUTES = 30;
     public const THREE_HOURS = 60*3;
-    public const TWELVE_HOURS = 60*12;
-    public const ONE_DAY = 60*24;
+//    public const TWELVE_HOURS = 60*12;
+    public const TODAY = 60*24-1;				// this one is an exception : shows only today, so we can not work with now() - x minutes
+    public const THREE_DAYS = 60*24*3;
+    public const ONE_DAY = 60*24-1;
     public const ONE_WEEK = 60*24*7;
     public const FOUR_WEEKS = 60*24*7*4;
     public const THREE_MONTHS = 60*24*31*3;
@@ -30,22 +32,30 @@ class TimeRanges
     public static function getValids(): array
     {
         return [
-            self::THIRTY_MINUTES => [
-                'label' => 'Thirty minutes',
-                'duration' => self::THIRTY_MINUTES
-            ],
+//            self::THIRTY_MINUTES => [
+//                'label' => 'Thirty minutes',
+//                'duration' => self::THIRTY_MINUTES
+//            ],
             self::THREE_HOURS => [
                 'label' => 'Three hours',
                 'duration' => self::THREE_HOURS
             ],
-            self::TWELVE_HOURS => [
-                'label' => 'Twelve hours',
-                'duration' => self::TWELVE_HOURS
+//            self::TWELVE_HOURS => [
+//                'label' => 'Twelve hours',
+//                'duration' => self::TWELVE_HOURS
+//            ],
+            self::TODAY => [
+                'label' => 'Today',
+                'duration' => self::TODAY
             ],
-            self::ONE_DAY => [
-                'label' => 'One day',
-                'duration' => self::ONE_DAY
+            self::THREE_DAYS => [
+                'label' => '72 hours',
+                'duration' => self::THREE_DAYS
             ],
+//            self::ONE_DAY => [
+//                'label' => 'One day',
+//                'duration' => self::ONE_DAY
+//            ],
             self::ONE_WEEK => [
                 'label' => 'One week',
                 'duration' => self::ONE_WEEK
